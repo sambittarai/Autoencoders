327be16a9f5f33de1164befe12103bda1541edb4
Sambit Tarai <sambitarai17@gmail.com>
2019-10-11 06:58:53 +0530

    Autoencoder

---
+++ main.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.datasets import mnist
+import numpy as np
+
