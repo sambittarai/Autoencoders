6b4783b17bb3d5b626deed1aca61ee34dff08457
Sambit Tarai <44292855+sambittarai@users.noreply.github.com>
2020-06-15 12:09:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
 from tensorflow.keras.datasets import mnist
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 #Parameters of the network
 depth = 32 #how much encoding to apply, compression of factor 24.5 if MNIST 28*28
 length = 28*28
 batch_size = 256
 
-
 #Network Architecture
-#create the network
 inputs = Input(shape=(length,))
 encoded = Dense(depth*4, activation='relu')(inputs)
 encoded = Dense(depth*2, activation='relu')(encoded)
@@ -27,13 +25,11 @@ autoencoder = Model(inputs, decoded)
 
 #create sub network
 encoder = Model(inputs, encoded)
-
 #Compile the model
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
 #load the data
 (x_train, _), (x_test, y_test) = mnist.load_data()
-
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
@@ -41,14 +37,9 @@ x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 print(x_train.shape)
 print(x_test.shape)
 
-
 #fit 
 autoencoder.fit(x_train, x_train, epochs=100, batch_size=batch_size, shuffle=True, validation_data=(x_test, x_test))
-
-
 #Plotting the input image and its corresponding Reconstructed image (Autoencoder's output)
-import matplotlib.pyplot as plt
-
 n = 10  # how many digits we will display
 plt.figure(figsize=(16, 4))
 for i in range(n):
@@ -66,7 +57,6 @@ for i in range(n):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 plt.show()
-
 
 #plot latent space  clustering
 x_encoded = encoder.predict(x_test, batch_size=batch_size)
